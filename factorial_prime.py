a5819aa6c6a9d1f15d62c95ad966ff28666f5a83
Saad Anees <145535412+Saad-Anees1@users.noreply.github.com>
2024-06-27 15:40:54 +0500

    Update factorial_prime.py

--- factorial_prime.py
+++ factorial_prime.py
@@ -3,13 +3,13 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n-1)   
 
 # Function to check if a number is prime
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):    
         if num % i == 0:
             return False
     return True
